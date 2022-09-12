ea28b8d63cf09c4ac9e78069c2e9e6d70c7648e4
Kepler Sticka-Jones <kepler@stickajones.org>
2022-09-11 18:06:33 -0600

    Move snapshot out of test

--- src/wikidata/wikidata-query.test.ts
+++ src/wikidata/wikidata-query.test.ts
@@ -22,23 +22,8 @@ describe("#queryWikidata", () => {
     LIMIT 10
     `);
 
-    expect(result).toMatchInlineSnapshot(`
-      Object {
-        "dataAttr1": "val11111",
-      }
-    `);
-    expect(mockedGot.mock.calls).toMatchInlineSnapshot(`
-      Array [
-        Array [
-          "https://query.wikidata.org/sparql?query=%0A%20%20%20%20SELECT%20DISTINCT%20%3Fitem%20%3FitemLabel%20%3FnpmPackageName%20WHERE%20%7B%0A%20%20%20%20%20%20%3Fitem%20wdt%3AP8262%20%3FnpmPackageName.%0A%20%20%20%20%20%20SERVICE%20wikibase%3Alabel%20%7B%20bd%3AserviceParam%20wikibase%3Alanguage%20%22%5BAUTO_LANGUAGE%5D%22.%20%7D%0A%20%20%20%20%7D%0A%20%20%20%20%0A%20%20%20%20LIMIT%2010%0A%20%20%20%20",
-          Object {
-            "headers": Object {
-              "Accept": "application/sparql-results+json",
-            },
-          },
-        ],
-      ]
-    `);
+    expect(result).toMatchSnapshot();
+    expect(mockedGot.mock.calls).toMatchSnapshot();
   });
 });
 
