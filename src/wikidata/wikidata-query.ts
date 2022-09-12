0319f31a23c718bedf0596d419105ae46da0b029
Kepler Sticka-Jones <kepler@stickajones.org>
2022-09-12 01:02:16 -0600

    Implementation and basic typing to get item from wikidata

--- src/wikidata/wikidata-query.ts
+++ src/wikidata/wikidata-query.ts
@@ -25,6 +25,72 @@ export async function queryWikidata<T extends QueryVariables>(
   ).json();
 }
 
+interface LocalizedString {
+  language: string;
+  value: string;
+}
+
+interface Snak {
+  snaktype: string;
+  property: string;
+  hash: string;
+  datavalue: {
+    value: string;
+    type: string;
+  };
+  datatype: string;
+}
+
+interface GetIDResult {
+  success: number;
+  entities: {
+    [id: string]: {
+      pageid: number;
+      ns: number;
+      title: string;
+      lastrevid: string;
+      modified: string;
+      type: string;
+      id: string;
+      labels: {
+        [lang: string]: LocalizedString;
+      };
+      descriptions: {
+        [lang: string]: LocalizedString;
+      };
+      aliases: {
+        [lang: string]: Array<LocalizedString>;
+      };
+      claims: {
+        [propId: string]: Array<{
+          mainsnak: Snak;
+          type: string;
+          id: string;
+          rank: string;
+          references: any[];
+        }>;
+      };
+      sitelinks: {
+        [site: string]: {
+          site: string;
+          title: string;
+          badges: any[];
+        };
+      };
+    };
+  };
+}
+
+export async function getItem(id: string): Promise<GetIDResult> {
+  return got(
+    `https://www.wikidata.org/w/api.php?${stringify({
+      action: "wbgetentities",
+      format: "json",
+      ids: id,
+    })}`
+  ).json();
+}
+
 interface ItemsWithPropertyOptions {
   limit?: number;
 }
